3697fe48c4c614f946251a2907dacdf7de24f2ae
eduardocd18 <javierecd@gmail.com>
2024-05-28 19:54:08 -0400

    Solucionado lo de las rutas para varias imágenes. Usa el htdocs de Apache

--- includes/templates/header.php
+++ includes/templates/header.php
@@ -4,10 +4,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- <link rel="preload" href="/bienesraices/build/css/app.css" as="style">
-    <link rel="stylesheet" href="/bienesraices/build/css/app.css"> -->
-    <link rel="preload" href="/build/css/app.css" as="style">
-    <link rel="stylesheet" href="/build/css/app.css">
+    <link rel="preload" href="/bienesraices/build/css/app.css" as="style">
+    <link rel="stylesheet" href="/bienesraices/build/css/app.css">
+    <!-- <link rel="preload" href="/build/css/app.css" as="style">
+    <link rel="stylesheet" href="/build/css/app.css"> -->
 </head>
 
 <body>
