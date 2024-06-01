29915a06da31d4979fb442636216fe3836dafcbf
eduardocd18 <javierecd@gmail.com>
2024-05-31 22:23:37 -0400

    puerto 3306 agregado para comprobar su funcionamiento

--- includes/config/database.php
+++ includes/config/database.php
@@ -1,6 +1,6 @@
 <?php
 function conectarDB(): mysqli
 {
-    $db = mysqli_connect('localhost', 'root', '', 'bienesraices_crud');
+    $db = mysqli_connect('localhost:3306', 'root', '', 'bienesraices_crud');
     return $db;
 }
