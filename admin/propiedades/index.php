85f37d8f7f132962fa9e77c7c0beb230a12e8478
eduardocd18 <javierecd@gmail.com>
2024-06-07 12:57:30 -0400

    322. Creando tabla para listar las propiedades de la base de datos

--- admin/propiedades/index.php
+++ admin/propiedades/index.php
@@ -16,6 +16,30 @@ incluirTemplate('header');
         <a href="crear.php" class="boton boton-verde">Nueva Propiedad →</a>
         <a href="crearVendedor.php" class="boton boton-verde">Nuevo Vendedor →</a>
     </div>
+
+    <table class="propiedades">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Título</th>
+                <th>Imagen</th>
+                <th>Precio</th>
+                <th>Acciones</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>1</td>
+                <td>Casa en la Playa</td>
+                <td><img src="../../imagenes/54a02849a70eb17d06bd44b162c94c35.jpg" alt="" class="imagen-tabla"></td>
+                <td>$345</td>
+                <td>
+                    <a href="#" class="boton-rojo-block">Eliminar</a>
+                    <a href="#" class="boton-amarillo-block">Actualizar</a>
+                </td>
+            </tr>
+        </tbody>
+    </table>
 </main>
 
 <?php
