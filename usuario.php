7e9ddc8749f969b1b4271d87357eade81250df69
eduardocd18 <javierecd@gmail.com>
2024-06-25 08:19:11 -0400

    333. Hashear Password

--- usuario.php
+++ usuario.php
@@ -7,8 +7,12 @@ $db = conectarDB();
 $email = "correo@correo.com";
 $password = "12345";
 
+// Hashar el password
+$passwordHash = password_hash($password, PASSWORD_BCRYPT);
+
+
 /* -- query para crear el usuario -- */
-$query = "INSERT INTO usuarios (email, password) VALUES ('$email', '$password');";
+$query = "INSERT INTO usuarios (email, password) VALUES ('$email', '$passwordHash');";
 
 /* --  Agregar a la Base de Datos -- */
 mysqli_query($db, $query);
